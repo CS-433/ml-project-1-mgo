75345826370832adabfeb7357e6a9509e8fd8190
bertrinm <maxence.bertringer@phelma.grenoble -inp.fr>
2021-10-22 10:35:19 +0200

    visualisation of the different features in order to see outliers and categorical features

---
+++ project1/scripts/helpers_perso.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""some personal helper functions for project 1."""
+import numpy as np
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+
+def de_standardize(x, mean_x, std_x):
+    """Reverse the procedure of standardization."""
+    x = x * std_x
+    x = x + mean_x
+    return x
